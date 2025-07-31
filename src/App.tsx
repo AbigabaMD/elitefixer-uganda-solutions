5979e28270441da18e6a02c7de9e09cef4efeaa5
abdqadir <masubaabdq@gmail.com>
2025-07-31 10:48:27 +0300

    app.tsx change again

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter >
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<Privacy />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/elitefixer-uganda-solutions/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
