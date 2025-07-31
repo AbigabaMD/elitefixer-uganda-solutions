81afda1f9a4ae34db1e0eeef3eb03a129f414d06
abdqadir <masubaabdq@gmail.com>
2025-07-31 10:41:31 +0300

    app.tsx change

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<Privacy />} />
